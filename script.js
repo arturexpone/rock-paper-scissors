27a10d4adb47718de97fd20acb6e05341d8964a1
arturexpone <arturexpone@yandex.ru>
2020-06-10 15:30:52 +0300

    added a random selection of options for the computer

--- script.js
+++ script.js
@@ -9,10 +9,29 @@ const game = () => {
 
         playBtn.addEventListener('click', (e) => {
             introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
         })
     }
 
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        options.forEach(option => {
+            option.addEventListener('click', function (e) {
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoise = computerOptions[computerNumber];
+                console.log(computerChoise)
+            })
+        })
+
+    }
+
     startGame();
+    playMatch();
 }
 
 game();
